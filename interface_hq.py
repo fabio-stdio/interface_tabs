7f1f88e6aa590fd3cb147982240d67291fbfec1b
fabio-stdio <fabio.stdio1@gmail.com>
2018-12-10 06:42:50 -0200

    Interface para o script py2sql_tabela_hq.py

---
+++ interface_hq.py
@@ -0,0 +1,15 @@
+from Tkinter import *
+
+
+class Quadrinhos:
+	def __init__(self, master=None)
+		self.fontepadrao = ("Arial", "10")
+		self.primeiroContainer = Frame(master)
+		self.primeiroContainer["pady"] = 10
+		self.primeiroContainer.pack()
+
+		
+
+root = Tk()
+Quadrinhos(root)
+root.mainloop)()
