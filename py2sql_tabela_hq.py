ea1b01fdb87c09deadac71e06611070d0b2afad1
fabio-stdio <fabio.stdio1@gmail.com>
2018-12-22 19:24:55 -0200

    Começou a ser construida a função insert_tab para inserção de dados na tabela do banco selecionada, primeiro para fim de teste

--- py2sql_tabela_hq.py
+++ py2sql_tabela_hq.py
@@ -5,9 +5,13 @@ import sqlite3
 
 #Conexão com a base de dados
 
-connection = sqlite3.connect("quadrinhos.db")
+connection = sqlite3.connect("test.db")
 
 #cursor
 
 cursor = connection.cursor()
 
+def insert_tab(char_t, int_n, char_col, char_ed, char_pub, char_con, char_f, char_l, int_ano, data, double_cp, double_est):
+	#Comando para o sql.
+
+	sql_command = """ INSERT INTO HQ VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
