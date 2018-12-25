6f77f2acf824fb4349d4683ca8e9e9149d9d4fdb
fabio-stdio <fabio.stdio1@gmail.com>
2018-12-25 05:49:07 -0200

    Foi terminado a função para inserir valores numa tabela teste em test

--- py2sql_tabela_hq.py
+++ py2sql_tabela_hq.py
@@ -3,15 +3,25 @@ import sqlite3
 
 
 
-#Conexão com a base de dados
-
-connection = sqlite3.connect("test.db")
-
-#cursor
-
-cursor = connection.cursor()
-
-def insert_tab(char_t, int_n, char_col, char_ed, char_pub, char_con, char_f, char_l, int_ano, data, double_cp, double_est):
+#def insert_tab(char_t, int_n, char_col, char_ed, char_pub, char_con, char_f, char_l, int_ano, data, double_cp, double_est)
+def insert_tab_teste(lista):
 	#Comando para o sql.
 
-	sql_command = """ INSERT INTO HQ VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+	#Conexão com a base de dados
+
+	connection = sqlite3.connect("test.db")
+
+	#cursor
+
+	cursor = connection.cursor()
+
+
+	#sql_command = """ INSERT INTO teste (coluna1, coluna2, coluna3, coluna4, coluna5) VALUES (?, ?, ?, ?, ?)"""
+	
+	cursor.executemany(""" INSERT INTO teste (coluna1, coluna2, coluna3, coluna4, coluna5) VALUES (?, ?, ?, ?, ?) """, lista)
+
+	connection.commit()
+
+	# Encerra a conexão com o DB.
+	connection.close()
+
