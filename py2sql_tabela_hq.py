aadcb927a2d3492ca8f1e76c6a6c07eafaa66dd2
fabio-stdio <fabio.stdio1@gmail.com>
2019-01-04 18:32:52 -0200

    Foi feita uma função para verificar a conexão com o banco de dados, mudança do nome da função de inserir na tabela, que ativa a conexão com o BD.

--- py2sql_tabela_hq.py
+++ py2sql_tabela_hq.py
@@ -1,24 +1,40 @@
 import sqlite3
+import os.path
+from sqlite3 import Error
 
-
-
+arquivo_db = 'test.db'
 
 #def insert_tab(char_t, int_n, char_col, char_ed, char_pub, char_con, char_f, char_l, int_ano, data, double_cp, double_est)
-def insert_tab_teste(lista):
-	#Comando para o sql.
+def conection(arquivo_db):
+	#Cria conexão para o banco de dados específico arquivo_db.
 
 	#Conexão com a base de dados
+	try:
 
-	connection = sqlite3.connect("test.db")
+		connection = sqlite3.connect(arquivo_db)
+		return connection
 
+	except Error as e:
+		print(e)
+
+	return None
+
+
+
+#def cria_tabela(parametros, tipo):
+	
+
+
+
+def tarefa_insere(lista):
 	#cursor
-
-	cursor = connection.cursor()
+	conn = conection(arquivo_db)
+	cursor = conn.cursor()
 
 
 	#sql_command = """ INSERT INTO teste (coluna1, coluna2, coluna3, coluna4, coluna5) VALUES (?, ?, ?, ?, ?)"""
 	
-	cursor.executemany(""" INSERT INTO teste (coluna1, coluna2, coluna3, coluna4, coluna5) VALUES (?, ?, ?, ?, ?) """, lista)
+	cursor.executemany(""" INSERT INTO mtg (carta, tipo, subtipo, raridade, cotação, data_cotação) VALUES (?, ?, ?, ?, ?, ?) """, lista)
 
 	connection.commit()
 
